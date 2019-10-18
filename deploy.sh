17b22e57a902787f1ce0df36a8d7af724c9e3d80
田翔 <tianxiang@shuidihuzhu.com>
2019-10-18 12:07:55 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+git add -A
+git commit -m 'deploy'
+
+git push origin master
+
 # 生成静态文件
 yarn run docs:build
 
@@ -11,10 +16,6 @@ cd dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
-
-# git init
-# git add -A
-# git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
